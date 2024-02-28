b9c1e2d205f7f953614092076a7747440bc897af
Scott Brooks <scott_brooks1998@hotmail.co.uk>
2024-02-28 14:14:41 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,29 @@
-const navSlide = () => {
-    const burger = document.querySelector(".burger");
+document.addEventListener("DOMContentLoaded", () => {
     const nav = document.querySelector(".nav-links");
-    const navLinks = document.querySelectorAll(".nav-links a");
+    // Initially hide the navigation links
+    nav.style.transform = "translateX(100%)";
 
-    burger.addEventListener("click", () => {
-        nav.classList.toggle("nav-active");
+    const navSlide = () => {
+        const burger = document.querySelector(".burger");
+        const navLinks = document.querySelectorAll(".nav-links a");
 
-        navLinks.forEach((link, index) => {
-            if (link.style.animation) {
-                link.style.animation = "";
-            } else {
-                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.5}s`;
-            }
+        burger.addEventListener("click", () => {
+            // Toggle nav
+            nav.classList.toggle("nav-active");
+
+            // Animate links
+            navLinks.forEach((link, index) => {
+                if (link.style.animation) {
+                    link.style.animation = "";
+                } else {
+                    link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`;
+                }
+            });
+
+            // Burger Animation
+            burger.classList.toggle("toggle");
         });
+    };
 
-        burger.classList.toggle("toggle");
-    });
-};
-
-navSlide();
+    navSlide();
+});
